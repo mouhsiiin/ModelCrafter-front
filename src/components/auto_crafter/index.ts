00ce34a6464d65e6498ff2c7de728df450082fd0
securdrgorp <zakaria.cherraj01@gmail.com>
2024-12-31 15:37:22 +0100

    Updated the AutoCrafter

--- src/components/auto_crafter/index.ts
+++ src/components/auto_crafter/index.ts
@@ -1,1 +1,2 @@
 export { default as AutoCrafter } from './AutoCrafter';
+export { default as JsonDisplay } from './JsonDisplay';
--- src/services/auto_model_crafter.ts
+++ src/services/auto_model_crafter.ts
@@ -1,11 +1,10 @@
 import api from "./api";
 
 
-export const getChartBar = async (file: any): Promise<JSON> => {
-    const response = await api.post("/auto/train", file, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
+export const craftData = async (file: any): Promise<JSON> => {
+    const response = await api.post("/auto/train", {
+      method: 'POST',
+      body: file
       });
     return response.data;
 };
